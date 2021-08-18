1ce1c3a0bd1c30d42f5438d85d4e7fd51a9ee1cd
Joao Victor <costa.jvsc@gmail.com>
2021-08-18 15:50:58 -0300

    dev: Create Product migrations

---
+++ Migrations/20210818184209_ProductAdd.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace djamba.Migrations
+{
+    public partial class ProductAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    price = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    slug = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
