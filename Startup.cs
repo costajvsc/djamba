d3344bfac33a46e98f16651f820e5b76cfc17b01
Joao Victor <costa.jvsc@gmail.com>
2021-08-18 20:09:57 -0300

    dev: Add CodeGeneration Desing Scaffold

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace djamba
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(strConnection));
             
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
