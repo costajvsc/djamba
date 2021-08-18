81e7f391615291f6c081367852384c325a7b51e6
Joao Victor <costa.jvsc@gmail.com>
2021-08-18 15:38:48 -0300

    dev: Configure DbContext on application

---
+++ Service/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using djamba.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace djamba.Service
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
